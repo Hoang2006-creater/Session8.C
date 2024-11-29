44cb76607ce23611c7835b3f7defc4aa9fea2b90
Hoang2006-creater <duhoang1611@gmail.com>
2024-11-29 14:40:28 +0700

    Add files via upload

---
+++ Session8-Lesson8.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Khai bao va gan gia tri 
+    int arr[3][3] = {{1, 2, 3}, {5, 6, 7},{9, 10, 11}};
+    int sum = 0;
+    printf("Cac phan tu trong duong cheo la :\n");
+    // In cac phan tu trong duong cheo 
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", arr[i][2-i]);  
+        sum += arr[i][2-i];  
+    }
+    // In ket qua 
+    printf("Tong cac gia tri tren duong treo la : %d\n", sum);
+    return 0;
+}
